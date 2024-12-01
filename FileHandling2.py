e0c6e7ccd3fb0b0b34e0f596054d947f321ab43b
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-12-02 05:07:40 +0530

    To read 'n' number of charcters from file using read(n).

--- FileHandling2.py
+++ FileHandling2.py
@@ -1,9 +1,16 @@
 #Reading character data from the file
-
+#using read()
 f=open('Rishi.txt','r')
 data=f.read() #read() reads total data from the file
 print("Reading total data completes....")
 print(data)
+f.close()
+
+#using read(n)
+f=open('Rishi.txt','r')
+char=f.read(10) #To read n charcters of data from file
+print("Reading only 10 number of characters completes....")
+print(char)
 f.close()
 
 
