16f51e9a2bbfd76bba5d7634bbb859547306c534
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-12-02 07:00:45 +0530

    program to take fileName from keyboard as input,check whether file exists or not,if exists print its content into console.

--- FileHandling5.py
+++ FileHandling5.py
@@ -1,14 +1,14 @@
 #Program to take fileName from keyboard as input,check whether file exists or not,if exists print its content
 import os
 fname=input("Enter the name of file:")
-with open(fname,'r') as f:
-	if os.path.isfile(fname):
-		print("File exists:",fname)
-		content=f.read()
-		print("Reading the content of file completes...")
-		print(content)
-		print("Printing the content of file to console completes...")
-	else:
-		print("File:",fname,"doesn't exists")
+if os.path.isfile(fname):
+    print("File exists:",fname)
+    with open(fname,'r') as f:
+        content=f.read()
+        print("Reading the content of file completes...")
+        print(content)
+        print("Printing the content of file to console completes...")
+else:
+    print("File:",fname,"doesn't exists")
 		
 		
