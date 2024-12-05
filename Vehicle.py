c6612d9dc1418a60316c04cf77d21a2373f9f130
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-12-05 15:53:27 +0530

    documentation string inside the class and how to access it using __doc__,help(classname).

---
+++ Vehicle.py
@@ -0,0 +1,6 @@
+class Vehicle:
+	'''Vehicle class is Developed by Rishabh''' #Content inside triple quotes is documentaton string
+print(Vehicle.__doc__)#documentation string can be accessed using classname.__doc__
+help(Vehicle) #help() returns the help about Vehicle class in bullet points
+	
+	
