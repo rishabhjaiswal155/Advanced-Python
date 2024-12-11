447e7abc2394a26cecafa5ee7a63d9cf7f391df9
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-12-11 16:46:26 +0530

    Public members can be access anywhere.

---
+++ Test17.py
@@ -0,0 +1,7 @@
+#Public member
+
+class Test17:
+	x=10
+	def __init__(self):
+		self.y=20 #Here x and y both are public 
+	
---
+++ Test18.py
@@ -0,0 +1,4 @@
+from Test17 import Test17
+t=Test17()
+print(t.x) #we can access public members anywhere.
+print(t.y) #we can access public members anywhere.
