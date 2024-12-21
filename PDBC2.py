246c9dc4de2d6a0d00e57ff8042ac5051a396506
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-12-21 18:34:53 +0530

    Python program to create a employees table in mysql database using pymysql module's connect(),execute().

---
+++ PDBC2.py
@@ -0,0 +1,7 @@
+#Python program to create a employees table in mysql database.
+import pymysql
+con=pymysql.connect(host='localhost',user='root',password='root123',database='python')
+cursor=con.cursor()
+query='create table employees(eno int(4) primary key,ename varchar(10),esal double(10,2),eaddr varchar(20))'
+cursor.execute(query)
+print("Table is created...")		
