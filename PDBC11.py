ba40ec76d683b5f9a7ec1266975ebb9a1b3c7f74
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-12-21 21:09:37 +0530

    Python program to fetch the employee data from mysql database and print on the console using pymysql module's fetchmany(n) which returns the tuple of n rows.

---
+++ PDBC11.py
@@ -0,0 +1,26 @@
+#Python program to fetch the employee data from mysql database and print on the console
+import pymysql
+try:
+    con=pymysql.connect(host='localhost',user='root',password='root123',database='python')
+    cursor=con.cursor()
+    query="select * from employees"
+    cursor.execute(query)
+    n=int(input("Enter the number of Employees for which data is to be fetched:"))
+    data=cursor.fetchmany(n)
+    for d in data:
+        print("Employee Number:",d[0])
+        print("Employee Name:",d[1])
+        print("Employee Salary:",d[2])
+        print("Employee Address:",d[3])
+        print()
+    print("Employee Data Fetched SuccessFully for",n,"Employees.")
+except pymysql.DatabaseError as m:
+    if con!=None:
+        con.rollback()
+        print("There is a problem:",m)
+finally:
+    if cursor:
+        cursor.close()
+    if con:
+        con.close()
+        
