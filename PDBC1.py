4b03c7b9e540c07ed1380b57d3b35bc41e64081b
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-12-21 18:38:04 +0530

    Closing the cursor and connection using close().

--- PDBC1.py
+++ PDBC1.py
@@ -1,9 +1,9 @@
-#Python Program to connect with mySql Database and print its Version
+#Python Program to connect with mySql Database.
 import pymysql
 con=pymysql.connect(host='localhost',user='root',password='root123',database='Python')
 if con!=None:
 	print("Connection established Successfully!!!")
 else:
 	print("Connection establishment failed..")
-
+con.close()
 
