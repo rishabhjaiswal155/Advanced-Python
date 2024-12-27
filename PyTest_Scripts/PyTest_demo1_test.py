be292ec6d3b234548e0598ef98ed1aec85ff198f
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-12-27 15:53:57 +0530

    PyTest Naming Conventions and how to run test cases using pyTest.

---
+++ PyTest_Scripts/PyTest_demo1_test.py
@@ -0,0 +1,11 @@
+#Naming convention for pytest:
+#1.filename should starts or ends with 'test'.
+#2.Class name should starts with 'Test'
+#3.Test method name should start with 'test_'.
+import pytest
+def test_method1():
+	print("demo1:test_method1() executes...")
+def test_method2():
+	print("demo1:test_method2() executes...")
+def test_method3():
+	print("demo1:test_method3() executes...")
---
+++ PyTest_Scripts/PyTest_demo2_test.py
@@ -0,0 +1,7 @@
+import pytest
+def test_method1():
+	print("demo2:test_method1() executes...")
+def test_method2():
+	print("demo2:test_method2() executes...")
+def test_method3():
+	print("demo2:test_method3() executes...")
